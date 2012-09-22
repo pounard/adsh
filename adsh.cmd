7c791c1393d38522b4f767cd38a96151f57062a8
Pierre Rineau <pierre.rineau@processus.org>
2012-09-22 12:42:07 +0200

    Removed useless stuff from Windows' CMD

--- adsh.cmd
+++ adsh.cmd
@@ -2,5 +2,4 @@
 rem This is copied from drush.bat Drush's script.
 rem set HOME=H:/adsh
 rem set TEMP=H:/adsh
-rem See http://drupal.org/node/506448 for more information.
-@php.exe "%~dp0drush.php" --php="php.exe" %*
+@php.exe "%~dp0drush.php" %*
